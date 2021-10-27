2fb1b68ec933fa61a7891beccc46335701993a95
Victor Afanassieff <victor@afanassieff.com>
2021-10-27 18:35:40 +0200

    feat: new eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,34 @@
 module.exports = {
-  extends: ['standard', 'prettier'],
-  plugins: ['import', 'prettier'],
+  extends: [
+    'standard',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:node/recommended',
+    'plugin:promise/recommended',
+  ],
+  plugins: ['import', 'prettier', 'promise'],
+  ignorePatterns: ['examples/**'],
   env: {
     node: true,
+    es2020: true,
+  },
+  globals: {
+    module: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
   rules: {
-    'space-before-function-paren': 0,
-    'new-cap': 0,
-    'prettier/prettier': 2,
-    camelcase: 0,
+    'space-before-function-paren': 'off',
+    'new-cap': 'off',
+    camelcase: 'off',
+    'import/no-unresolved': ['error', { commonjs: true }],
+    'import/no-extraneous-dependencies': 'error',
+    'node/shebang': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'node/no-missing-require': 'off',
   },
 }
