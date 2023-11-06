273ee06ad806a590b54d18789ee4b2e0060014ef
Sitt Guruvanich <aekazitt@gmail.com>
2023-11-06 17:03:53 +0700

    Test fetching futures ticker

---
+++ tests/rest/ticker.spec.ts
@@ -0,0 +1,30 @@
+/* ~~/vitest.config.ts */
+
+// imports
+import { createRestClient } from '../../src/rest'
+import { beforeAll, describe, it, expect } from 'vitest'
+import { randomBytes } from 'node:crypto'
+
+// persistence
+let lnmarkets: ReturnType<typeof createRestClient>
+let network: string
+
+beforeAll(() => {
+  network = 'testnet'
+  lnmarkets = createRestClient({ network })
+})
+
+describe('Fetches latest ticker', () => {
+  it('Retrieves latest testnet ticker with four number parameters', async () => {
+    const { carryFeeTimestamp, carryIndicativeFeeRate, index, lastPrice } =
+      await lnmarkets.futuresGetTicker()
+    expect(carryFeeTimestamp).toBeTruthy() // non-zero and non-null
+    expect(carryFeeTimestamp).toBeTypeOf('number')
+    expect(carryIndicativeFeeRate).toBeTruthy() // non-zero and non-null
+    expect(carryIndicativeFeeRate).toBeTypeOf('number')
+    expect(index).toBeTruthy() // non-zero and non-null
+    expect(index).toBeTypeOf('number')
+    expect(lastPrice).toBeTruthy() // non-zero and non-null
+    expect(lastPrice).toBeTypeOf('number')
+  })
+})
---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+/* ~~/vitest.config.ts */
+
+// imports
+import { defineConfig } from 'vitest/config'
+
+// https://vitest.dev/guide/#configuring-vitest
+export default defineConfig({
+  test: {
+    include: ['tests/**/*.spec.ts'],
+  },
+})
