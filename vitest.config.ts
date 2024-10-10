3f6d9f6873f08ce379957a17d7546044f801a14f
Victor Afanassieff <victor@afanassieff.com>
2024-10-10 13:58:17 +0200

    chore: vitest should look for .test.ts files

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['tests/**/*.spec.ts'],
+    include: ['./**/*.test.ts'],
     poolOptions: {
       threads: {
         singleThread: true,
